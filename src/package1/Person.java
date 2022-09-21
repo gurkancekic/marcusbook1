8217b4ee6066b66441f4e5310813bbb895101be5
gurkancekic <gurkancekic@gmail.com>
2022-09-21 17:00:50 +0300

    ilk github yuklemem , hayırlısıyla.

---
+++ src/package1/Person.java
@@ -0,0 +1,7 @@
+package package1;
+
+public class Person {
+    public static void main(String[] args){}
+    public String name;
+
+}
---
+++ src/package2/PersonTest.java
@@ -0,0 +1,38 @@
+package package2;
+import org.junit.Assert;
+import org.junit.Test;
+import package1.Person;
+
+import static org.junit.Assert.assertEquals;
+
+public class PersonTest {
+
+    @Test
+    public void main() {
+        Person[] person;
+        person = new Person[12];
+        // person1=new Person();
+        int i=0;
+
+        while (i<4) {
+            person[i]=new Person();
+            i++;
+        }
+        person[3].name = "hakan";
+        person[2].name = "levent";
+        assertEquals("hakan", person[3].name);
+        Assert.assertNotSame(person[3],person[2]);
+        Assert.assertNotNull(person[3]);
+        Assert.assertNull(person[9]);
+         // Person gurkan32350 = new Person();
+        //Person levent35500 = new Person();
+        //System.out.println("Hello world!");
+        /**
+        if (person[3] != null && person[2] != null){
+            System.out.println("person2 mevcut ismi :  " + person[2].name );
+            System.out.println("person3 mevcut ismi :  " + person[3].name );
+        }**/
+    }
+
+}
+
